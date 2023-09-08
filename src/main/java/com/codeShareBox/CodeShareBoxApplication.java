6aff3b17b24f523ea74bb7dc6373ffe86d09ab96
Md Farhan Anwar <mdfarhananwar@gmail.com>
2023-09-08 21:31:27 +0530

    Added ComponentScan

--- src/main/java/com/codeShareBox/CodeShareBoxApplication.java
+++ src/main/java/com/codeShareBox/CodeShareBoxApplication.java
@@ -2,10 +2,11 @@ package com.codeShareBox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@ComponentScan(basePackages = "com.codeShareBox")
 public class CodeShareBoxApplication {
 
 	public static void main(String[] args) {
